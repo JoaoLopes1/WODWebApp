ab40ed98857e1aae4aa62aba6d59ffffc158708c
guizzoS2 <guilvieira409@gmail.com>
2023-10-22 16:36:23 -0300

    added modal (future login modal)

---
+++ Projeto WOD/scripts/script.js
@@ -0,0 +1,27 @@
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal")
+const fade = document.querySelector("#fade")
+
+const toggleModal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((
+el) => {
+    el.addEventListener("click", () => toggleModal());
+});
+
+
+
+const logged = false
+
+function verify() {
+
+    return logged
+}
+
+function openProfile() {
+    
+}
