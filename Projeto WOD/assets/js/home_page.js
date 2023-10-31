b0e64b86015ca459cc8ca8a03785f13cfee8a1c0
joaolopes1 <joaopedro.lopes05@gmail.com>
2023-10-31 16:14:25 -0300

    Nova responsividade da home page

--- Projeto WOD/assets/js/home_page.js
+++ Projeto WOD/assets/js/home_page.js
@@ -20,3 +20,24 @@ function desselecionarBotao() {
 const button_iconSelecionado = document.querySelector(".button_icon.selecionado");
 button_iconSelecionado.classList.remove("selecionado");
 }
+
+// NavBar Hamburber
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
