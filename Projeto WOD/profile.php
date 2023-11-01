788b645bfddd248464b6a8fa02d25473c590b795
guizzoS2 <guilvieira409@gmail.com>
2023-11-01 16:16:23 -0300

    deleting copies

--- Projeto WOD/profile.php
+++ Projeto WOD/profile.php
@@ -54,7 +54,7 @@ if (isset($_POST["username"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/profile2.css">
+    <link rel="stylesheet" href="assets/css/pages.css">
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Profile2</title>  
